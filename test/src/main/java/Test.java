f0c4ae5e977f57c7f6f6d14038a818c774625556
chenchen207@aliyun.com <chenchen207@aliyun.com>
2018-12-01 14:41:48 +0800

    tianjiaceshi daima

--- test/src/main/java/Test.java
+++ test/src/main/java/Test.java
@@ -1,9 +1,9 @@
 public class Test {
 
     public static void main(String[] args){
-        System.out.print("ddddddddddddddddddddd");
-        System.out.print("ddddddddddddddddddddd");
-        System.out.print("ddddddddddddddddddddd");
+        System.out.print("对方告诉对方说如果还是");
+        System.out.print("啥地方不还是地方不少地方");
+        System.out.print("的发生不少地方吧");
 
     }
 }
